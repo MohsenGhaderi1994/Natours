73c9ffb458a4b0f4302968c63331500fe8c3385a
Mohsen <mohsenghaderi.1994@gmail.com>
2022-11-22 14:03:06 -0500

    Added different error handling for production and dev env

--- controllers/globalErrorHandler.js
+++ controllers/globalErrorHandler.js
@@ -1,9 +1,27 @@
 const CONSTS = require('./../HTTP_RESPONSE_CODES');
-module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || CONSTS.HTTP_INTERNAL_SERVER_ERROR;
-    err.status = err.status || 'error';
+const sendErrorDev = (req, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProduction = (req, res) => {
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message
     });
+};
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || CONSTS.HTTP_INTERNAL_SERVER_ERROR;
+    err.status = err.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProduction(err, res);
+    }
 };
